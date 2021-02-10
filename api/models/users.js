9cb8f772abd02d3fbaf86808ee012ba9e4196301
pguimarais <pguimarais@telaria.com>
2021-02-10 13:23:32 -0800

    workaround for user sessions

--- api/models/users.js
+++ api/models/users.js
@@ -6,38 +6,73 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({
 })
 
 const findOrCreate = async ({ id, username, displayName }) => {
+  // step 1: set all the users as not current user
+  // step 2: set current user as "currentUser = true"
+  
   try {
     const existingUser = await getUserByTwitterId(id)
+
+    const scanParams = {
+      TableName: "users-table-dev-twitter-users"
+    }
+
+    const users = (await dynamodb.scan(scanParams).promise())
+    
+    if (users && users.Items) {
+      Promise.all(
+        users.Items.map(async (user) => {
+          const userId = user.user_id 
+
+          const updateParam = {
+            TableName: "users-table-dev-twitter-users",
+            Item: {
+              user_id: userId,
+              currentUser: false 
+            }
+          }
+
+          let result = await dynamodb.put(updateParam).promise()
+          console.log('update result: ', result)
+        })
+      )
+    }
   
     if (existingUser) {
+      // user exists - lets update to the current
       console.log('existing user: ', existingUser)
       
-      const scanParams = {
-        TableName: "users-table-dev-twitter-users"
+      const updateCurrent = {
+        TableName: "users-table-dev-twitter-users",
+        Item: {
+          twitter_id: id,
+          currentUser: true
+        }
       }
 
-      const users = await dynamodb.scan(scanParams).promise()
-      console.log('users: ', users)
+      const updateExistingUser = await dynamodb.put(updateCurrent).promise()
+      console.log('update existing user: ', updateExistingUser)
+      return existingUser;
 
-      return existingUser
+    } else {
+      // user does not exist yet - let's add to table as current user 
 
-      // then we map through, construct put requests, then fire off a Promise.all() call 
-    }
+      const params = {
+        TableName: "users-table-dev-twitter-users",
+        Item: {
+          twitter_id: id,
+          user_id: uuidv4(),
+          username,
+          display_name: displayName,
+          currentUser: true,
+          createdAt: Date.now(),
+          updatedAt: Date.now()
+        }
+      }
   
-    const params = {
-      TableName: "users-table-dev-twitter-users",
-      Item: {
-        twitter_id: id,
-        user_id: uuidv4(),
-        username,
-        display_name: displayName,
-        createdAt: Date.now(),
-        updatedAt: Date.now()
-      }
+      console.log('creating user with twitter_id: ', id)  
+      const newUser = await dynamodb.put(params).promise()     
+      console.log('new user created: ', newUser)
     }
-
-    console.log('creating user with twitter_id: ', id)  
-    return await dynamodb.put(params).promise()     
   } catch (error) {
     console.log('error with findOrCreate: ', error)
   }
